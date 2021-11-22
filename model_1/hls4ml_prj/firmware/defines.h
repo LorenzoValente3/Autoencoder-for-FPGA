8b44d77ba433c0f6dee070bcccc172088ae0f7ed
Lorenzo Valente <lorenzo.valente3@studio.unibo.it>
2021-11-22 15:37:58 +0100

    ae compile hls4ml

---
+++ model_1/hls4ml_prj/firmware/defines.h
@@ -0,0 +1,35 @@
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+#include "ap_int.h"
+#include "ap_fixed.h"
+#include "nnet_utils/nnet_types.h"
+#include <cstddef>
+#include <cstdio>
+
+//hls-fpga-machine-learning insert numbers
+#define N_INPUT_1_1 784
+#define N_LAYER_2 128
+#define N_LAYER_4 64
+#define N_LAYER_6 2
+#define N_LAYER_8 64
+#define N_LAYER_10 128
+#define N_LAYER_12 784
+
+//hls-fpga-machine-learning insert layer-precision
+typedef ap_fixed<16,6> model_default_t;
+typedef ap_fixed<16,6> input_t;
+typedef ap_fixed<16,6> layer2_t;
+typedef ap_fixed<16,6> layer3_t;
+typedef ap_fixed<16,6> layer4_t;
+typedef ap_fixed<16,6> layer5_t;
+typedef ap_fixed<16,6> layer6_t;
+typedef ap_fixed<16,6> layer7_t;
+typedef ap_fixed<16,6> layer8_t;
+typedef ap_fixed<16,6> layer9_t;
+typedef ap_fixed<16,6> layer10_t;
+typedef ap_fixed<16,6> layer11_t;
+typedef ap_fixed<16,6> layer12_t;
+typedef ap_fixed<16,6> result_t;
+
+#endif
---
+++ model_1/hls4ml_prj/firmware/weights/b6.h
@@ -0,0 +1,15 @@
+//Numpy array shape [2]
+//Min 0.127385094762
+//Max 0.324288964272
+//Number of zeros 0
+
+#ifndef B6_H_
+#define B6_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b6[2];
+#else
+model_default_t b6[2] = {0.1273850948, 0.3242889643};
+#endif
+
+#endif
