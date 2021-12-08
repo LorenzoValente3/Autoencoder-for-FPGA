c1b28cbcc7e99c2a520b78416147470270666ad8
Lorenzo Valente <lorenzo.valente3@studio.unibo.it>
2021-12-08 20:53:55 +0100

    update

---
+++ model/AE_model/hls4ml_prj/firmware/parameters.h
@@ -0,0 +1,193 @@
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+#include "ap_int.h"
+#include "ap_fixed.h"
+
+#include "nnet_utils/nnet_helpers.h"
+//hls-fpga-machine-learning insert includes
+#include "nnet_utils/nnet_activation.h"
+#include "nnet_utils/nnet_activation_stream.h"
+#include "nnet_utils/nnet_dense.h"
+#include "nnet_utils/nnet_dense_compressed.h"
+#include "nnet_utils/nnet_dense_stream.h"
+ 
+//hls-fpga-machine-learning insert weights
+#include "weights/w2.h"
+#include "weights/b2.h"
+#include "weights/w4.h"
+#include "weights/b4.h"
+#include "weights/w6.h"
+#include "weights/b6.h"
+#include "weights/w8.h"
+#include "weights/b8.h"
+#include "weights/w10.h"
+#include "weights/b10.h"
+#include "weights/w12.h"
+#include "weights/b12.h"
+
+//hls-fpga-machine-learning insert layer-config
+// dense_73
+struct config2 : nnet::dense_config {
+    static const unsigned n_in = N_INPUT_1_1;
+    static const unsigned n_out = N_LAYER_2;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 3200;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+    template<class x_T, class y_T, class res_T>
+    using product = nnet::product::mult<x_T, y_T, res_T>;
+};
+
+// dense_73_relu
+struct relu_config3 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+// dense_74
+struct config4 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_2;
+    static const unsigned n_out = N_LAYER_4;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 512;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+    template<class x_T, class y_T, class res_T>
+    using product = nnet::product::mult<x_T, y_T, res_T>;
+};
+
+// dense_74_relu
+struct relu_config5 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_4;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+// encoder_output
+struct config6 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_4;
+    static const unsigned n_out = N_LAYER_6;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 32;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+    template<class x_T, class y_T, class res_T>
+    using product = nnet::product::mult<x_T, y_T, res_T>;
+};
+
+// encoder_output_relu
+struct relu_config7 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_6;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+// dense_75
+struct config8 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_6;
+    static const unsigned n_out = N_LAYER_8;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 32;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+    template<class x_T, class y_T, class res_T>
+    using product = nnet::product::mult<x_T, y_T, res_T>;
+};
+
+// dense_75_relu
+struct relu_config9 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_8;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+// dense_76
+struct config10 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_8;
+    static const unsigned n_out = N_LAYER_10;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 512;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+    template<class x_T, class y_T, class res_T>
+    using product = nnet::product::mult<x_T, y_T, res_T>;
+};
+
+// dense_76_relu
+struct relu_config11 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_10;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+// ecoder_output
+struct config12 : nnet::dense_config {
+    static const unsigned n_in = N_LAYER_10;
+    static const unsigned n_out = N_LAYER_12;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned strategy = nnet::latency;
+    static const unsigned reuse_factor = 1;
+    static const unsigned n_zeros = 0;
+    static const unsigned n_nonzeros = 3200;
+    static const bool store_weights_in_bram = false;
+    typedef ap_fixed<16,6> accum_t;
+    typedef model_default_t bias_t;
+    typedef model_default_t weight_t;
+    typedef ap_uint<1> index_t;
+    template<class x_T, class y_T, class res_T>
+    using product = nnet::product::mult<x_T, y_T, res_T>;
+};
+
+// ecoder_output_sigmoid
+struct sigmoid_config13 : nnet::activ_config {
+    static const unsigned n_in = N_LAYER_12;
+    static const unsigned table_size = 1024;
+    static const unsigned io_type = nnet::io_parallel;
+    static const unsigned reuse_factor = 1;
+    typedef ap_fixed<18,8> table_t;
+};
+
+
+#endif
---
+++ model/AE_model/hls4ml_prj/firmware/weights/b6.h
@@ -0,0 +1,15 @@
+//Numpy array shape [2]
+//Min 0.077838614583
+//Max 0.306184202433
+//Number of zeros 0
+
+#ifndef B6_H_
+#define B6_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b6[2];
+#else
+model_default_t b6[2] = {0.0778386146, 0.3061842024};
+#endif
+
+#endif
