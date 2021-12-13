6edd735e470176d7d11a241545816631afb7d34f
LorenzoValente3 <lorenzo.valente3@studio.unibo.it>
2021-12-13 10:54:30 +0100

    autoencoder

--- MNIST_dataset.py
+++ MNIST_dataset.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import mnist as mnist
 from tensorflow.python.keras.utils.np_utils import to_categorical
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,19 +23,11 @@ class MNISTData:
         self.get_subset_of_data(data_fraction)
 
         self.convert_label_to_categorical()
-        
-        #self.normalize_mnist_images()
-        
-        
-
+                
         self.crop_and_interpolate(size_initial, size_final, color_depth)
-          
-        #self.reshape_to_color_channel()
-
-        #self.crop_center( 22)
         
-        #if flat is True:
-        #    self.flatten_pictures()
+        if flat is True:
+            self.flatten_pictures()
 
     def get_subset_of_data(self, data_fraction):
         """Choosing a fraction of data according to the machine capabilities"""
@@ -51,7 +43,9 @@ class MNISTData:
         self.y_test = to_categorical(self.y_test)
 
     def crop_and_interpolate(self, size_initial, size_final, color_depth):
-        #defisce gli indici del cropping definito da size_initial
+        """Definition of the crop indeces defined by the input variable size_inizial.
+            The dataset is then shrinked and zoomed according the input values."""
+        
         bordo = (28-size_initial)//2
         bordo_top = -bordo
         if bordo == 0:
@@ -62,10 +56,11 @@ class MNISTData:
         
         X_train_flat_zoom_int = []
         X_test_flat_zoom_int = []
-
+        
+        #processing train set
         for image in self.x_train:
             tmp = scipy.ndimage.zoom(image[bordo:bordo_top, bordo:bordo_top],
-                                     size_final/size_initial).flatten()
+                                     size_final/size_initial)
             tmp = (tmp/(256//2**color_depth)).astype(int)
             X_train_flat_zoom.append(tmp/2**color_depth)
             X_train_flat_zoom_int.append(tmp)
@@ -75,10 +70,10 @@ class MNISTData:
 
         self.x_train=X_train_flat_zoom
 
-        #processing Test Set
+        #processing test set
         for image in self.x_test:
             tmp = scipy.ndimage.zoom(image[bordo:bordo_top, bordo:bordo_top], 
-                                        size_final/size_initial).flatten()
+                                        size_final/size_initial)
             tmp = (tmp/(256//2**color_depth)).astype(int)
             X_test_flat_zoom.append(tmp/2**color_depth)
             X_test_flat_zoom_int.append(tmp)
@@ -87,20 +82,6 @@ class MNISTData:
         X_test_flat_zoom_int = np.array(X_test_flat_zoom_int)
         self.x_test=X_test_flat_zoom
 
-
-
-
-
-    def normalize_mnist_images(self):
-        self.x_train = self.x_train / 255.0
-        self.x_test = self.x_test / 255.0
-
-    def reshape_to_color_channel(self):
-        self.x_train = self.x_train[:, :, :, np.newaxis]
-        self.x_test = self.x_test[:, :, :, np.newaxis]
-
-    
-    
     def flatten_pictures(self):
         self.x_train = self.x_train.reshape(self.x_train.shape[0], -1)
         self.x_test = self.x_test.reshape(self.x_test.shape[0], -1)
